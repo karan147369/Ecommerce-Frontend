875a76699411f39e3c79816f505a53dae7eb8492
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-11-19 02:05:45 +0530

    comments added

--- src/api/apiLogin.js
+++ src/api/apiLogin.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const url = "https://karan147369-ecommerce-backend.onrender.com/login";
+//"https://karan147369-ecommerce-backend.onrender.com/login"
+//http://localhost:4000/login/
 const apiLogin = async (email, password) => {
   const response = await axios.post(url + "email", {
     email,
